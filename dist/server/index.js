7b3fdca87146dbcfbbaece48810fb6d75066118b
WSminnie <WSminnie@users.noreply.github.com>
2026-07-29 20:43:05 +0700

    Revert "Add social-friendly clean URL redirects"
    This reverts commit 6a08941be21651e0f03d29d7b1d9ab720e221a01.

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,21 +1,5 @@
 export default {
   async fetch(request, env) {
-    const cleanRouteMap = {
-      "/auausave": "/#/AUAUSAVE",
-      "/auautnp": "/#/AUAU",
-      "/savewrg": "/#/SAVE",
-      "/projects": "/#/projects",
-    };
-    const url = new URL(request.url);
-    const pathname = url.pathname.replace(/\/+$/, "").toLowerCase();
-    const targetRoute = cleanRouteMap[pathname];
-
-    if (targetRoute && !url.hash) {
-      const targetUrl = new URL(targetRoute, url.origin);
-      targetUrl.search = url.search;
-      return Response.redirect(targetUrl.toString(), 302);
-    }
-
     return env.ASSETS.fetch(request);
   },
 };
