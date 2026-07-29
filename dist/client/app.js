d128f569e96b9055e2758e6905db2917cab954f7
WSminnie <WSminnie@users.noreply.github.com>
2026-07-29 20:43:05 +0700

    Revert "Support legacy slash projects hash route"
    This reverts commit ed9e73f5b48589a46a37ab0d0ebd9627a74e6624.

--- dist/client/app.js
+++ dist/client/app.js
@@ -2275,7 +2275,6 @@ function router() {
   )
     listing(route);
   else if (route.startsWith("artist/")) profile(artistIdFromPublicRoute(route.slice(7)));
-  else if (route === "/projects") projectHubPage();
   else if (route.startsWith("/")) profile(artistIdFromPublicRoute(route));
   else if (route === "projects") projectHubPage();
   else if (route.startsWith("project/")) projectDetailPage(route.slice(8));
